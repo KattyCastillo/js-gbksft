a3487c7ce4461e6e2c88a11d4d93f8d3190eaa0d
KattyCastillo <stephany7castillo@gmail.com>
2022-03-24 17:18:31 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-gbksft
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-gbksft)
---
+++ index.js
@@ -0,0 +1,20 @@
+// Import stylesheets
+// import './style.css';
+
+// Write Javascript code!
+// const appDiv = document.getElementById('app');
+// appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+var nombre = 'Carlos';
+console.log('Hi, how are you?');
+
+var numero = 1000;
+console.log(numero);
+
+var objeto = {
+  nombres: 'Katty',
+  apellidos: 'Castillo Galindo',
+  telefono: '9837-2683',
+};
+
+console.log(objeto);
